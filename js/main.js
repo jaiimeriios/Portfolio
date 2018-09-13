2fc9ec7febbfec9de391e3165360852d54f57378
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2018-09-13 11:00:44 -0700

    spacio

--- js/main.js
+++ js/main.js
@@ -384,17 +384,6 @@
 			url: cfg.mailChimpURL
 		});
 
-		// Mailchimp translation
-		//
-		//  Defaults:
-		//	 'submit': 'Submitting...',
-		//  0: 'We have sent you a confirmation email',
-		//  1: 'Please enter a value',
-		//  2: 'An email address must contain a single @',
-		//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-		//  4: 'The username portion of the email address is invalid (the portion before the @: )',
-		//  5: 'This email address looks fake or invalid. Please enter a real email address'
-
 		$.ajaxChimp.translations.es = {
 			'submit': 'Submitting...',
 			0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
@@ -428,7 +417,6 @@
 		});
 	};
 
-
 /* Initialize
 	* ------------------------------------------------------ */
 	(function ssInit() {
@@ -448,6 +436,5 @@
 		clBackToTop();
 
 	})();
-
 
 })(jQuery);
