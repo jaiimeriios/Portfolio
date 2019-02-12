5c924a048ef9a4793f1cb5a7f621631bba354a93
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-02-12 12:29:56 -0800

    onscroll

--- js/main.js
+++ js/main.js
@@ -484,23 +484,7 @@ I8aaaaaa8aaa8baaaaaa88aaaaaaaa8aaaaaaaa88aaaaaad8aaa8aaaaaa8I
 	    });
     };
 
-    /* Show Nav on Scroll up --------------------------------------------- */
-    var clShowNav = function() {
-
-        var prevScrollpos = window.pageYOffset;
-                
-        window.onscroll = function() {
-
-            var currentScrollPos = window.pageYOffset;
-
-            if (prevScrollpos > currentScrollPos) {
-                document.getElementById("navbar").style.top = "0px";
-            } else {
-                document.getElementById("navbar").style.top = "-100px";
-            }
-            prevScrollpos = currentScrollPos;
-        }
-    };
+    
 
 
    /* Initialize ------------------------------------------------------ */
@@ -521,7 +505,6 @@ I8aaaaaa8aaa8baaaaaa88aaaaaaaa8aaaaaaaa88aaaaaad8aaa8aaaaaa8I
         clAjaxChimp();
         clBackToTop();
         clCleanURL();
-        clShowNav();
     })();
         
 })(jQuery);
