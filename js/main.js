1d48cde1b731cae20c19bdbfb2d84745a3088a99
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-04-18 18:15:54 -0700

    forma

--- js/main.js
+++ js/main.js
@@ -195,46 +195,43 @@
 
 	   /* Contact Form
     * ------------------------------------------------------ */
-//    var clContactForm = function() {
-        
-	// $('#contactForm').validate({
+	// var clContactForm = function() {
+		
+	// 	$('#contactForm').validate({
+	// 		submitHandler: function(form) {
+	// 			var sLoader = $('.submit-loader');
+	// 			$.ajax({
+	// 				type: "POST",
+	// 				url: "inc/sendEmail.php",
+	// 				data: $(form).serialize(),
+	// 				beforeSend: function() { 
+	// 					sLoader.slideDown("slow");
+	// 				},
+	// 				success: function(msg) {
+	// 					// Message was sent
+	// 					if (msg == 'OK') {
+	// 						sLoader.slideUp("slow"); 
+	// 						$('.message-warning').fadeOut();
+	// 						$('#contactForm').fadeOut();
+	// 						$('.message-success').fadeIn();
+	// 					}
+	// 					// There was an error
+	// 					else {
+	// 						sLoader.slideUp("slow"); 
+	// 						$('.message-warning').html(msg);
+	// 						$('.message-warning').slideDown("slow");
+	// 					}
+	// 				},
+	// 				error: function() {
+	// 					sLoader.slideUp("slow"); 
+	// 					$('.message-warning').html("Something went wrong. Please try again.");
+	// 					$('.message-warning').slideDown("slow");
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+	// };
 
-	// 	submitHandler: function(form) {
-
-	// 		var sLoader = $('.submit-loader');
-
-			// $.ajax({
-
-			// 	type: "POST",
-			// 	url: "inc/sendEmail.php",
-			// 	data: $(form).serialize(),
-			// 	beforeSend: function() { 
-			// 		sLoader.slideDown("slow");
-			// 	},
-			// 	success: function(msg) {
-			// 		// Message was sent
-			// 		if (msg == 'OK') {
-			// 			sLoader.slideUp("slow"); 
-			// 			$('.message-warning').fadeOut();
-			// 			$('#contactForm').fadeOut();
-			// 			$('.message-success').fadeIn();
-			// 		}
-			// 		// There was an error
-			// 		else {
-			// 			sLoader.slideUp("slow"); 
-			// 			$('.message-warning').html(msg);
-			// 			$('.message-warning').slideDown("slow");
-			// 		}
-			// 	},
-			// 	error: function() {
-			// 		sLoader.slideUp("slow"); 
-			// 		$('.message-warning').html("Something went wrong. Please try again.");
-			// 		$('.message-warning').slideDown("slow");
-			// 	}
-			// });
-// 		}
-// 	});
-// };
 
    /* Initialize ------------------------------------------------------ */
     (function ssInit() {
