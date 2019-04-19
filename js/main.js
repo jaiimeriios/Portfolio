2064ed4b008a442b3e68e79b0825ec42ecbd07fa
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-04-18 17:03:27 -0700

    fsadg

--- js/main.js
+++ js/main.js
@@ -131,38 +131,42 @@
         $('#contactForm').validate({
 
             submitHandler: function(form) {
+	
+				
+// Message was sent
+if (msg == 'OK') {
+	sLoader.slideUp("slow"); 
+	$('.message-warning').fadeOut();
+	$('#contactForm').fadeOut();
+	$('.message-success').fadeIn();
+}
+// There was an error
+else {
+	sLoader.slideUp("slow"); 
+	$('.message-warning').html(msg);
+	$('.message-warning').slideDown("slow");
+}
+
+
+                // var sLoader = $('.submit-loader');
     
-                var sLoader = $('.submit-loader');
+                // $.ajax({
     
-                $.ajax({
-    
-                    // type: "POST",
-                    // url: "inc/sendEmail.php",
-                    //data: $(form).serialize(),
-                    beforeSend: function() { 
-                        sLoader.slideDown("slow");
-                    },
-                    success: function(msg) {
-                        // Message was sent
-                        if (msg == 'OK') {
-                            sLoader.slideUp("slow"); 
-                            $('.message-warning').fadeOut();
-                            $('#contactForm').fadeOut();
-                            $('.message-success').fadeIn();
-                        }
-                        // There was an error
-                        else {
-                            sLoader.slideUp("slow"); 
-                            $('.message-warning').html(msg);
-                            $('.message-warning').slideDown("slow");
-                        }
-                    },
-                    error: function() {
-                        sLoader.slideUp("slow"); 
-                        $('.message-warning').html("Something went wrong. Please try again.");
-                        $('.message-warning').slideDown("slow");
-                    }
-            	});
+                //     // type: "POST",
+                //     // url: "inc/sendEmail.php",
+                //     data: $(form).serialize(),
+                //     beforeSend: function() { 
+                //         sLoader.slideDown("slow");
+                //     },
+                //     success: function(msg) {
+                        
+                //     },
+                //     error: function() {
+                //         sLoader.slideUp("slow"); 
+                //         $('.message-warning').html("Something went wrong. Please try again.");
+                //         $('.message-warning').slideDown("slow");
+                //     }
+            	// });
             }
         });
     };
