ca76e5c738bf7d3a41f79dba800dab0660b01d29
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-04-18 17:26:08 -0700

    k;jjkl;'

--- js/main.js
+++ js/main.js
@@ -125,6 +125,62 @@
     };
 
 
+<<<<<<< HEAD
+=======
+   /* Contact Form
+    * ------------------------------------------------------ */
+    var clContactForm = function() {
+        
+        /* local validation */
+        $('#contactForm').validate({
+        
+            /* submit via ajax */
+            submitHandler: function(form) {
+    
+                var sLoader = $('.submit-loader');
+    
+                $.ajax({
+    
+                    type: "POST",
+                    url: "inc/sendEmail.php",
+                    data: $(form).serialize(),
+                    beforeSend: function() { 
+    
+                        sLoader.slideDown("slow");
+    
+                    },
+                    success: function(msg) {
+    
+                        // Message was sent
+                        if (msg == 'OK') {
+                            sLoader.slideUp("slow"); 
+                            $('.message-warning').fadeOut();
+                            $('#contactForm').fadeOut();
+                            $('.message-success').fadeIn();
+                        }
+                        // There was an error
+                        else {
+                            sLoader.slideUp("slow"); 
+                            $('.message-warning').html(msg);
+                            $('.message-warning').slideDown("slow");
+                        }
+    
+                    },
+                    error: function() {
+    
+                        sLoader.slideUp("slow"); 
+                        $('.message-warning').html("Something went wrong. Please try again.");
+                        $('.message-warning').slideDown("slow");
+    
+                    }
+    
+                });
+            }
+    
+        });
+    };
+
+>>>>>>> parent of e2691dc... required
 
    /* Animate On Scroll
     * ------------------------------------------------------ */
@@ -201,6 +257,10 @@
         clOffCanvas();
         clSmoothScroll();
         clAlertBoxes();
+<<<<<<< HEAD
+=======
+        clContactForm();
+>>>>>>> parent of e2691dc... required
         clAOS();
         clBackToTop();
         clShowNavL();
