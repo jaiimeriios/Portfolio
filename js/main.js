dc8651f9ff5e06198c2bb13aeb8951e66f988de3
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-04-18 17:40:19 -0700

    noajax

--- js/main.js
+++ js/main.js
@@ -203,35 +203,35 @@
 
 			var sLoader = $('.submit-loader');
 
-			$.ajax({
+			// $.ajax({
 
-				// type: "POST",
-				// url: "inc/sendEmail.php",
-				// data: $(form).serialize(),
-				// beforeSend: function() { 
-				// 	sLoader.slideDown("slow");
-				// },
-				success: function(msg) {
-					// Message was sent
-					if (msg == 'OK') {
-						sLoader.slideUp("slow"); 
-						$('.message-warning').fadeOut();
-						$('#contactForm').fadeOut();
-						$('.message-success').fadeIn();
-					}
-					// There was an error
-					else {
-						sLoader.slideUp("slow"); 
-						$('.message-warning').html(msg);
-						$('.message-warning').slideDown("slow");
-					}
-				},
-				error: function() {
-					sLoader.slideUp("slow"); 
-					$('.message-warning').html("Something went wrong. Please try again.");
-					$('.message-warning').slideDown("slow");
-				}
-			});
+			// 	type: "POST",
+			// 	url: "inc/sendEmail.php",
+			// 	data: $(form).serialize(),
+			// 	beforeSend: function() { 
+			// 		sLoader.slideDown("slow");
+			// 	},
+			// 	success: function(msg) {
+			// 		// Message was sent
+			// 		if (msg == 'OK') {
+			// 			sLoader.slideUp("slow"); 
+			// 			$('.message-warning').fadeOut();
+			// 			$('#contactForm').fadeOut();
+			// 			$('.message-success').fadeIn();
+			// 		}
+			// 		// There was an error
+			// 		else {
+			// 			sLoader.slideUp("slow"); 
+			// 			$('.message-warning').html(msg);
+			// 			$('.message-warning').slideDown("slow");
+			// 		}
+			// 	},
+			// 	error: function() {
+			// 		sLoader.slideUp("slow"); 
+			// 		$('.message-warning').html("Something went wrong. Please try again.");
+			// 		$('.message-warning').slideDown("slow");
+			// 	}
+			// });
 		}
 	});
 };
