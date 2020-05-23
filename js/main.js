965236cdc1e3574189d2e21207f3ebaa0e7c58e8
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2020-05-23 15:14:50 -0700

    space

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
         $("html").addClass('cl-preload');
         $WIN.on('load', function() {
             // force page scroll position to top at page refresh
-        // $('html, body').animate({ scrollTop: 0 }, 'normal');
+            // $('html, body').animate({ scrollTop: 0 }, 'normal');
             // will first fade out the loading animation 
             $("#loader").fadeOut("slow", function() {
                 // will fade out the whole DIV that covers the website.
